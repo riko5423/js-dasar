eee66306d0d3012e290a7c18d57d2623b3f80e73
rikorahmamaulana <rikorm28@gmail.com>
2023-02-20 23:23:44 +0700

    menambahkan escape sequence pada tipe data string

--- tipe-data-string.js
+++ tipe-data-string.js
@@ -4,3 +4,9 @@ console.log("Programmer Zaman Now");
 
 // menambah string / string concat
 console.log("Nama : " + "Riko " + "Rahma " + "Maulana");
+
+// escape sequence
+console.log("Riko \nRahma \nMaulana");
+console.log("\\Home\\Maulana");
+console.log('"Programmer Zaman Now"');
+console.log("'Programmer Zaman Now'");
