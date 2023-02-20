c7893b71db35661b2282f92ffbda40d2c205325d
rikorahmamaulana <rikorm28@gmail.com>
2023-02-20 23:14:11 +0700

    menambahkan tipe data string

---
+++ tipe-data-string.js
@@ -0,0 +1,6 @@
+// tipe data string
+console.log("Riko Rahma Maulana");
+console.log("Programmer Zaman Now");
+
+// menambah string / string concat
+console.log("Nama : " + "Riko " + "Rahma " + "Maulana");
