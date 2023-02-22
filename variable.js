ee253513dd1e4f0a090e029f7238bd8e8f1b9ea5
rikorahmamaulana <rikorm28@gmail.com>
2023-02-22 19:38:52 +0700

    menambahkan variable

---
+++ variable.js
@@ -0,0 +1,30 @@
+// variable
+// tempat untuk menyimpan data
+
+var fullName;
+
+fullName = "Riko Rahma Maulana";
+
+console.log(fullName);
+
+var a;
+var b;
+
+a = 10;
+b = 10;
+
+console.log(a + b);
+
+var firstName;
+var middleName;
+var lastName;
+
+firstName = "Riko";
+middleName = "Rahma";
+lastName = "Maulana";
+
+console.log(firstName, middleName, lastName);
+
+// membuat variable langsung dengan isi value nya
+var alamat = "Cikarang";
+console.log(alamat);
