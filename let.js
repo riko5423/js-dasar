e66d34f85f7d61acfadfffbdabf54b6f8576b010
rikorahmamaulana <rikorm28@gmail.com>
2023-02-25 20:04:25 +0700

    menambahkan let

---
+++ let.js
@@ -0,0 +1,30 @@
+// variable
+// tempat untuk menyimpan data
+
+let fullName;
+
+fullName = "Riko Rahma Maulana";
+
+console.log(fullName);
+
+let a;
+let b;
+
+a = 10;
+b = 10;
+
+console.log(a + b);
+
+let firstName;
+let middleName;
+let lastName;
+
+firstName = "Riko";
+middleName = "Rahma";
+lastName = "Maulana";
+
+console.log(firstName, middleName, lastName);
+
+// membuat variable langsung dengan isi value nya
+var alamat = "Cikarang";
+console.log(alamat);
